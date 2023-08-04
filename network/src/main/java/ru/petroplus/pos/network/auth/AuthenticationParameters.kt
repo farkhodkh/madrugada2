beb164ca14da6e7d5886db7ee10d5ef6fb6f7c94
farkhod khaknazarov <f.khaknazarov@avtoapp.ru>
2023-08-04 13:55:03 +0300

    SSL Connection  Success state

--- network/src/main/java/ru/petroplus/pos/network/auth/AuthenticationParameters.kt
+++ network/src/main/java/ru/petroplus/pos/network/auth/AuthenticationParameters.kt
@@ -2,6 +2,9 @@ package ru.petroplus.pos.network.auth
 
 import java.io.File
 
+/**
+ * Класс для идентификации по SSL
+ */
 class AuthenticationParameters {
     lateinit var clientCertificate: File
     val clientCertificatePassword: String = "1234"
