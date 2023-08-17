4994f70063966d141be7adbf6dc40894e475e16b
denis.vidanov <Denis.Vidanov@transitcard.ru>
2023-08-17 15:30:14 +0300

    CP-312 - Исправлен баг с некорректным состоянием экранов. Рефакторинг экрана Debug

--- mainscreen/src/main/java/ru/petroplus/pos/mainscreen/ui/debit/DebitViewState.kt
+++ mainscreen/src/main/java/ru/petroplus/pos/mainscreen/ui/debit/DebitViewState.kt
@@ -1,12 +1,12 @@
 package ru.petroplus.pos.mainscreen.ui.debit
 
-import ru.petroplus.pos.debug.DebitDebugGroup
+import ru.petroplus.pos.mainscreen.ui.debit.debug.DebitDebugGroup
 
 sealed class DebitViewState {
     object StartingState: DebitViewState()
     sealed class DebugState: DebitViewState() {
         object APDU : DebugState()
-        data class Debit(val debitDebugGroup: DebitDebugGroup) : DebugState()
+        data class Debit(val debitDebugGroup: DebitDebugGroup = DebitDebugGroup()) : DebugState()
 
     }
     object InsertCartState: DebitViewState()
