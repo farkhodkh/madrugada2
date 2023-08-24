1771b844d50203d28084371cfad93e763f16fef7
denis.vidanov <Denis.Vidanov@transitcard.ru>
2023-08-24 14:52:26 +0300

    CP-314 Доработан запрос чека дебета. Теперь используются Relation вместо сложного запроса

--- room/src/main/java/ru/petrolplus/pos/room/dao/ReceiptDao.kt
+++ room/src/main/java/ru/petrolplus/pos/room/dao/ReceiptDao.kt
@@ -12,26 +12,9 @@ interface ReceiptDao {
 
     /**
      * Комбинированный запрос из 4х таблиц (transactions, receipt_params, common_settings и services)
-     * ищент по id определенную транзакцию, забирает из таблиц common_settings и receipt_number единственные
-     * значения и ищет данные сервиса по id соответствующему service_id_what из транзакции
-     * @param transactionId идентификатор транзакции
-     * @return проекцию из TransactionDB, ServiceDB, CommonSettingsDB и ReceiptParamsDB, может быть
-     * null если не найдено транзакций с таким id
+     * ищет по id определенную транзакцию ограничивает поиск только одним результатом. Создает фейковую колонку
+     * со значением 1 для внешних связей через механизм OneToMany room см [ReceiptProjection]
      */
-    @Query("SELECT * FROM (SELECT * FROM transactions WHERE id = :transactionId LIMIT 1) " +
-            "CROSS JOIN (SELECT " +
-                "id as r_id, " +
-                "current_receipt_number as r_current_receipt_number FROM receipt_params LIMIT 1) " +
-            "CROSS JOIN (SELECT " +
-                "id as c_id, " +
-                "organization_name as c_organization_name, " +
-                "organization_inn as c_organization_inn, " +
-                "pos_name as c_pos_name FROM common_settings LIMIT 1) " +
-            "LEFT JOIN (SELECT " +
-                "id as s_id, " +
-                "name as s_name, " +
-                "unit as s_unit, " +
-                "price as s_price from services) ON service_id_what = s_id"
-    )
+    @Query("SELECT *, 1 as relation_id FROM transactions WHERE id = :transactionId LIMIT 1")
     fun getDebitReceiptByTransactionId(transactionId: String): ReceiptProjection?
 }
