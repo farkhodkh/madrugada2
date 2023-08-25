d96315734244613213cd344ca7fb0bce8582cd72
denis.vidanov <Denis.Vidanov@transitcard.ru>
2023-08-25 16:07:30 +0300

    CP-337 Добавлен метод для получения последней транзакции по id карты и услуги

--- room/src/androidTest/java/ru/petrolplus/pos/TransactionsTests.kt
+++ room/src/androidTest/java/ru/petrolplus/pos/TransactionsTests.kt
@@ -4,33 +4,33 @@ import android.content.Context
 import androidx.room.Room
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import org.junit.After
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 import ru.petrolplus.pos.room.database.AppDatabase
-import ru.petrolplus.pos.room.entities.CommonSettingsDB
-import ru.petrolplus.pos.room.entities.ReceiptParamsDB
-import ru.petrolplus.pos.room.entities.ServiceDB
 import ru.petrolplus.pos.room.entities.TransactionDB
 import java.util.Calendar
 
 @RunWith(AndroidJUnit4::class)
-class ReceiptProjectionTest {
+class TransactionsTests {
     
     private val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
     private val db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).build()
     private val transactionsDao = db.transactionDao()
-    private val commonSettingsDao = db.commonSettingsDao()
-    private val receiptParamsDao = db.receiptParamsDao()
-    private val servicesDao = db.servicesDao()
-    private val receiptDao = db.receiptDao()
+
+    @After
+    fun reset() {
+        db.clearAllTables()
+    }
     
     @Test
-    fun testGetDebitReceipt() {
+    fun testGetLastTransactionByCardNumberAndServiceId() {
         runBlocking {
             transactionsDao.insert(TransactionDB(
-                id = "ABCDEFGIDSLKJDIFELSKJKJKDSFDIJSD",
+                id = "ABCDEFGIDSLKJDIFELSKJKJKDSFOOBTB",
                 acquirerId = 1005,
                 terminalId = 2,
                 cardNumber = "1005000000",
@@ -56,16 +56,37 @@ class ReceiptProjectionTest {
                 responseCode = 19
             ))
 
-            commonSettingsDao.insert(CommonSettingsDB(1, "Первая автокомпания", "111222333444", "АЗС №1"))
-            receiptParamsDao.insert(ReceiptParamsDB(1, 3))
-            servicesDao.insert(ServiceDB(1, "АИ-95", "Л", 45000))
+            delay(2000)
 
-            val receipt = receiptDao.getDebitReceiptByTransactionId("ABCDEFGIDSLKJDIFELSKJKJKDSFDIJSD")
+            transactionsDao.insert(TransactionDB(
+                id = "ABCDEFGIDSLKJDIFELSKJKJKDSFBBDDD",
+                acquirerId = 1005,
+                terminalId = 2,
+                cardNumber = "1005000000",
+                operatorNumber = "4000123456",
+                terminalDate = Calendar.getInstance(),
+                serviceIdWhat = 1,
+                serviceIdFrom = 3,
+                amount = 2147483647000,
+                price = 2147483647000,
+                sum = 2147483647000,
+                cardTransactionCounter = 2147483647000,
+                hasReturn = false,
+                crs32 = "DJIFESLD",
+                operationType = 1,
+                cardType = 2,
+                loyaltySum = 2147483647000,
+                deltaBonus = 2147483647000,
+                originalDebitTransactionId = "",
+                shiftNumber = 30,
+                hasRecalculationTransaction = false,
+                rollbackCode = "IELSBMDSOOKKHUEL",
+                receiptNumber = 2147483647000,
+                responseCode = 19
+            ))
 
-            Assert.assertEquals(receipt?.transactionDB?.id, "ABCDEFGIDSLKJDIFELSKJKJKDSFDIJSD")
-            Assert.assertEquals(receipt?.receiptParamsDB?.id, 1)
-            Assert.assertEquals(receipt?.commonSettingsDB?.id, 1)
-            Assert.assertEquals(receipt?.serviceDB?.id, 1)
+            val transactionDB = transactionsDao.getLastByCardNumberAndService("1005000000", 1)
+            Assert.assertEquals(transactionDB?.id, "ABCDEFGIDSLKJDIFELSKJKJKDSFBBDDD")
         }
 
     }
