462d1d4c6d5e819d80303e3348bdd02a413548df
yuriy.poprygushin <yuriy.poprygushin@pprcard.ru>
2023-08-26 00:22:57 +0300

    CP-327_Merging_refactoring

--- p7libapi/src/main/java/ru/petroplus/pos/p7LibApi/dto/ClockDto.kt
+++ p7libapi/src/main/java/ru/petroplus/pos/p7LibApi/dto/ClockDto.kt
@@ -1,11 +1,22 @@
 package ru.petroplus.pos.p7LibApi.dto
 
+/**
+ * Класс представления времени
+ * @property year - год, отсчитывается от 1900 г.
+ * @property month - месяц, 1=JAN ...  12=DEC
+ * @property day - день месяца, 1 ... 28/29/30/31
+ * @property hour - часы, 0 ... 23
+ * @property minute - минуты, 0 ... 59
+ * @property second - секунды, 0 ... 59
+ * @property weekDay - день недели, 1=MON ... 7=SUN
+ */
+
 class ClockDto {
-    var year : Short = 0              /* annee     0=1900 ... 255=2155    */
-    var month : Short = 0             /* mois      1=JAN  ...  12=DEC     */
-    var day : Short = 0               /* jour      1      ... 28/29/30/31 */
-    var hour : Short = 0              /* heure     0      ...  23         */
-    var minute : Short = 0            /* minute    0      ...  59         */
-    var second : Short = 0            /* seconde   0      ...  59         */
-    var weekDay : Short = 0  /* jour de la semaine 1=LUN  ...   7=DIM     */
+    var year : Short = 0
+    var month : Short = 0
+    var day : Short = 0
+    var hour : Short = 0
+    var minute : Short = 0
+    var second : Short = 0
+    var weekDay : Short = 0
 }
