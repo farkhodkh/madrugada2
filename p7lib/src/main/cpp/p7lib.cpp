4728bb6ab100f6068ccaa7ef11d7fb3ed2c2f640
Хакназаров Фарход Ражаббоевич <farkhod.khaknazarov@pprcard.ru>
2023-08-31 10:28:54 +0300

    HOT-FIX - renamed package name

--- p7lib/src/main/cpp/p7lib.cpp
+++ p7lib/src/main/cpp/p7lib.cpp
@@ -12,48 +12,48 @@ const char *TAG = "P7Lib";
 
 extern "C"
 JNIEXPORT jobject JNICALL
-Java_ru_petroplus_pos_p7Lib_impl_P7LibRepositoryImpl_init(JNIEnv *env, jobject thiz,
+Java_ru_petrolplus_pos_p7Lib_impl_P7LibRepositoryImpl_init(JNIEnv *env, jobject thiz,
                                                           jobject init_data, jstring last_op_guid,
                                                           jobject callbacks, jstring temp_dir,
                                                           jstring data_dir) {
-    jclass resultClass = env->FindClass("ru/petroplus/pos/p7LibApi/dto/OK");
+    jclass resultClass = env->FindClass("ru/petrolplus/pos/p7LibApi/dto/OK");
     jmethodID method = env->GetMethodID(resultClass, "<init>", "()V");
     return env->NewObject(resultClass, method);
 }
 extern "C"
 JNIEXPORT jobject JNICALL
-Java_ru_petroplus_pos_p7Lib_impl_P7LibRepositoryImpl_deInit(JNIEnv *env, jobject thiz) {
+Java_ru_petrolplus_pos_p7Lib_impl_P7LibRepositoryImpl_deInit(JNIEnv *env, jobject thiz) {
     jstring dfg = 0;
 }
 extern "C"
 JNIEXPORT jobject JNICALL
-Java_ru_petroplus_pos_p7Lib_impl_P7LibRepositoryImpl_detect(JNIEnv *env, jobject thiz,
+Java_ru_petrolplus_pos_p7Lib_impl_P7LibRepositoryImpl_detect(JNIEnv *env, jobject thiz,
                                                             jobject card_key, jobject card_data) {
     jstring dfg = 0;
 }
 extern "C"
 JNIEXPORT jobject JNICALL
-Java_ru_petroplus_pos_p7Lib_impl_P7LibRepositoryImpl_debit(JNIEnv *env, jobject thiz,
+Java_ru_petrolplus_pos_p7Lib_impl_P7LibRepositoryImpl_debit(JNIEnv *env, jobject thiz,
                                                            jobject params, jobject info,
                                                            jstring uuid) {
     jstring dfg = 0;
 }
 extern "C"
 JNIEXPORT jobject JNICALL
-Java_ru_petroplus_pos_p7Lib_impl_P7LibRepositoryImpl_refund(JNIEnv *env, jobject thiz,
+Java_ru_petrolplus_pos_p7Lib_impl_P7LibRepositoryImpl_refund(JNIEnv *env, jobject thiz,
                                                             jobject params, jobject info,
                                                             jstring uuid) {
     jstring dfg = 0;
 }
 extern "C"
 JNIEXPORT jobject JNICALL
-Java_ru_petroplus_pos_p7Lib_impl_P7LibRepositoryImpl_getErrorInfo(JNIEnv *env, jobject thiz,
+Java_ru_petrolplus_pos_p7Lib_impl_P7LibRepositoryImpl_getErrorInfo(JNIEnv *env, jobject thiz,
                                                                   jobject error_info) {
     jstring dfg = 0;
 }
 extern "C"
 JNIEXPORT jobject JNICALL
-Java_ru_petroplus_pos_p7Lib_impl_P7LibRepositoryImpl_getLibInfo(JNIEnv *env, jobject thiz,
+Java_ru_petrolplus_pos_p7Lib_impl_P7LibRepositoryImpl_getLibInfo(JNIEnv *env, jobject thiz,
                                                                 jobject lib_info) {
     jstring dfg = 0;
 }
