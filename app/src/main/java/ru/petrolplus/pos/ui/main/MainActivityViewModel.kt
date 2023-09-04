1bc6099a49b5b8e3c2357df0030df7bc01e0bd77
yuriy.poprygushin <yuriy.poprygushin@pprcard.ru>
2023-09-04 17:29:12 +0300

    CP-327_Merging_refactoring

--- app/src/main/java/ru/petrolplus/pos/ui/main/MainActivityViewModel.kt
+++ app/src/main/java/ru/petrolplus/pos/ui/main/MainActivityViewModel.kt
@@ -74,34 +74,6 @@ class MainActivityViewModel(
             )
         }
 
-////==================================================================================================
-//
-//        val UUID = TransactionUUIDDto()
-//        UUID.onlineTransNumber = 18
-//        UUID.lastGenTime = 1690547808
-//        UUID.clockSequence = 61920
-//        UUID.hasNodeId = true
-//        UUID.nodeId = ubyteArrayOf(0x01u, 0xB5u, 0x14u, 0x6Fu, 0xB4u, 0xE3u).toByteArray()
-//
-//        var initData = InitDataDto()
-//        initData.acquirerId = 4000
-//        initData.terminalId = 111
-//        initData.hostIp = "127.0.0.1"
-//        initData.hostPort = 5676
-//
-//        var DataDirectoryPath = Environment.getStorageDirectory().getAbsolutePath();
-//        DataDirectoryPath += File.separator + "P7Lib";
-//
-//        var result = p7LibraryRepository.init(
-//            initData,
-//            UUID,
-//            callbacks,
-//            DataDirectoryPath,
-//            DataDirectoryPath
-//        )
-//
-////==================================================================================================
-
         _viewState.value = MainScreenState.CheckingSuccessState
     }
 
